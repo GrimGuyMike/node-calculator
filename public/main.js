3c8aecbcc94a3d0b2ab0bbdac7e1b578498a0402
GrimGuyMike <ggm_job@mail.ru>
2021-09-07 19:11:59 +0500

    added wrapper main function

--- public/main.js
+++ public/main.js
@@ -1,304 +1,310 @@
-// Objects setup
+function main() {
 
-const screen = function(){
+    // Objects setup
 
-    let history = document.querySelector('.screen > .history');
-    let current = document.querySelector('.screen > .current');
+    const screen = function(){
 
-    return {
+        let history = document.querySelector('.screen > .history');
+        let current = document.querySelector('.screen > .current');
 
-        doReset: false,
+        return {
 
-        clear(){
-            current.innerText = 0;
-            history.innerText = '';
-        },
+            doReset: false,
 
-        clearEntry(){
-            current.innerText = 0;
-        },
+            clear(){
+                current.innerText = 0;
+                history.innerText = '';
+            },
 
-        appendNumber(val){
+            clearEntry(){
+                current.innerText = 0;
+            },
 
-            if(this.doReset){
-                this.clear();
-                this.doReset = false;
+            appendNumber(val){
+
+                if(this.doReset){
+                    this.clear();
+                    this.doReset = false;
+                }
+
+                let entry = this.getEntry();
+
+                switch(val){
+
+                    case '.': {
+                        if(entry.includes(val)){
+                            return;
+                        } else {
+                            break;
+                        }
+                    };
+
+                    case '0': {
+                        if(entry.startsWith(val) && !entry.includes('.')) return;
+                    };
+
+                    default: {
+                        if(entry.startsWith('0') && !entry.includes('.')){
+                            entry = entry.substring(0, entry.length - 1);
+                        }
+                    };
+
+                }
+
+                entry = entry + val;
+                current.innerText = entry;
+
+            },
+
+            toggleSign(){
+
+                let entry = this.getEntry();
+
+                if(entry.startsWith('0') && !entry.includes('.')){
+                    return;
+                }
+                
+                if(entry.startsWith('-')){
+                    entry = entry.substring(1, entry.length);
+                } else {
+                    entry = '-' + entry;
+                }
+
+                this.clearEntry();
+                this.appendNumber(entry);
+
+            },
+
+            erase(){
+
+                let entry = this.getEntry();
+
+                if(entry.length == 1){
+                    entry = 0;
+                } else {
+                    entry = entry.substring(0, entry.length - 1);
+                }
+
+                this.clearEntry();
+                this.appendNumber(entry);
+
+            },
+
+            writeToHistory(val){
+                history.innerText = val;
+            },
+
+            getEntry(){
+                return current.innerText;
+            },
+
+            getHistory(){
+                return history.innerText;
             }
 
-            let entry = this.getEntry();
+        }
 
-            switch(val){
+    }();
 
-                case '.': {
-                    if(entry.includes(val)){
-                        return;
-                    } else {
-                        break;
-                    }
-                };
+    const calculator = {
 
-                case '0': {
-                    if(entry.startsWith(val) && !entry.includes('.')) return;
-                };
+        opd1: undefined,
+        opd2: undefined,
+        op: undefined,
 
-                default: {
-                    if(entry.startsWith('0') && !entry.includes('.')){
-                        entry = entry.substring(0, entry.length - 1);
-                    }
-                };
+        operation(op){
 
+            if(screen.doReset){
+                screen.doReset = false;
             }
 
-            entry = entry + val;
-            current.innerText = entry;
-
-        },
-
-        toggleSign(){
-
-            let entry = this.getEntry();
-
-            if(entry.startsWith('0') && !entry.includes('.')){
-                return;
-            }
+            let primOps = ['+', '-', '*', '/'];
+            let specOps = ['%', 'rec', 'sqr', 'sqrt']; // percentage, reciprocal, square, square root
             
-            if(entry.startsWith('-')){
-                entry = entry.substring(1, entry.length);
-            } else {
-                entry = '-' + entry;
-            }
+            if(primOps.includes(op)){
 
-            this.clearEntry();
-            this.appendNumber(entry);
+                if(this.opd1 != undefined){
 
-        },
+                    this.opd2 = Number(screen.getEntry());
+                    let result = eval(this.opd1 + this.op + this.opd2);
+                    this.opd1 = result;
+                    this.op = op;
+                    screen.writeToHistory(this.opd1 + this.op);
+                    this.opd2 = undefined;
 
-        erase(){
+                } else {
 
-            let entry = this.getEntry();
+                    this.opd1 = Number(screen.getEntry());
+                    this.op = op;
+                    screen.writeToHistory(this.opd1 + this.op);
 
-            if(entry.length == 1){
-                entry = 0;
-            } else {
-                entry = entry.substring(0, entry.length - 1);
-            }
+                }
 
-            this.clearEntry();
-            this.appendNumber(entry);
-
-        },
-
-        writeToHistory(val){
-            history.innerText = val;
-        },
-
-        getEntry(){
-            return current.innerText;
-        },
-
-        getHistory(){
-            return history.innerText;
-        }
-
-    }
-
-}();
-
-const calculator = {
-
-    opd1: undefined,
-    opd2: undefined,
-    op: undefined,
-
-    operation(op){
-
-        if(screen.doReset){
-            screen.doReset = false;
-        }
-
-        let primOps = ['+', '-', '*', '/'];
-        let specOps = ['%', 'rec', 'sqr', 'sqrt']; // percentage, reciprocal, square, square root
-        
-        if(primOps.includes(op)){
-
-            if(this.opd1 != undefined){
-
-                this.opd2 = Number(screen.getEntry());
-                let result = eval(this.opd1 + this.op + this.opd2);
-                this.opd1 = result;
-                this.op = op;
-                screen.writeToHistory(this.opd1 + this.op);
-                this.opd2 = undefined;
-
-            } else {
-
-                this.opd1 = Number(screen.getEntry());
-                this.op = op;
-                screen.writeToHistory(this.opd1 + this.op);
+                screen.clearEntry();
+                return;
 
             }
 
-            screen.clearEntry();
-            return;
+            if(specOps.includes(op)){
 
-        }
+                let entry = Number(screen.getEntry());
 
-        if(specOps.includes(op)){
+                switch(op){
 
-            let entry = Number(screen.getEntry());
+                    case '%': {
 
-            switch(op){
+                        if(this.op == '/'){
 
-                case '%': {
+                            let expression = this.opd1 + this.op + entry;
+                            let result = eval(expression) * 100;
+                            this.clear();
+                            screen.clear();
+                            screen.writeToHistory(expression + '=(%)');
+                            screen.appendNumber(result);
+                            screen.doReset = true;
 
-                    if(this.op == '/'){
+                        } else {
 
-                        let expression = this.opd1 + this.op + entry;
-                        let result = eval(expression) * 100;
-                        this.clear();
+                            entry /= 100;
+                            screen.clearEntry();
+                            screen.appendNumber(entry);
+                            
+                        }
+
+                        break;
+
+                    };
+
+                    case 'rec': {
+
                         screen.clear();
-                        screen.writeToHistory(expression + '=(%)');
-                        screen.appendNumber(result);
+                        screen.writeToHistory('1/' + entry + '=');
+                        entry **= -1;
+                        screen.appendNumber(entry);
                         screen.doReset = true;
 
-                    } else {
+                        break;
 
-                        entry /= 100;
-                        screen.clearEntry();
+                    };
+
+                    case 'sqr': {
+
+                        screen.clear();
+                        screen.writeToHistory('sqr(' + entry + ')=');
+                        entry **= 2;
                         screen.appendNumber(entry);
-                        
-                    }
+                        screen.doReset = true;
 
-                    break;
+                        break;
 
-                };
+                    };
 
-                case 'rec': {
+                    case 'sqrt': {
 
-                    screen.clear();
-                    screen.writeToHistory('1/' + entry + '=');
-                    entry **= -1;
-                    screen.appendNumber(entry);
-                    screen.doReset = true;
+                        screen.clear();
+                        screen.writeToHistory('sqrt(' + entry + ')=');
+                        entry **= .5;
+                        screen.appendNumber(entry);
+                        screen.doReset = true;
 
-                    break;
+                    };
 
-                };
-
-                case 'sqr': {
-
-                    screen.clear();
-                    screen.writeToHistory('sqr(' + entry + ')=');
-                    entry **= 2;
-                    screen.appendNumber(entry);
-                    screen.doReset = true;
-
-                    break;
-
-                };
-
-                case 'sqrt': {
-
-                    screen.clear();
-                    screen.writeToHistory('sqrt(' + entry + ')=');
-                    entry **= .5;
-                    screen.appendNumber(entry);
-                    screen.doReset = true;
-
-                };
+                }
 
             }
 
+        },
+
+        compute(){
+            
+            let expression = screen.getHistory() + screen.getEntry();
+            let result = eval(expression);
+            calculator.clear();
+            screen.clear();
+            screen.writeToHistory(expression + '=');
+            screen.appendNumber(result);
+
+            screen.doReset = true;
+
+        },
+
+        clear(){
+
+            this.opd1 = undefined;
+            this.opd2 = undefined;
+            this.op = undefined;
+
         }
 
-    },
+    };
 
-    compute(){
-        
-        let expression = screen.getHistory() + screen.getEntry();
-        let result = eval(expression);
-        calculator.clear();
-        screen.clear();
-        screen.writeToHistory(expression + '=');
-        screen.appendNumber(result);
+    // Number keys
 
-        screen.doReset = true;
-
-    },
-
-    clear(){
-
-        this.opd1 = undefined;
-        this.opd2 = undefined;
-        this.op = undefined;
-
+    for(let i=0; i<10; i++){
+        document.querySelector(`.button._${i}`).addEventListener('click', function(){
+            screen.appendNumber(this.innerText);
+        });
     }
 
-};
+    // Point key
 
-// Number keys
-
-for(let i=0; i<10; i++){
-    document.querySelector(`.button._${i}`).addEventListener('click', function(){
+    document.querySelector('.button.point').addEventListener('click', function(){
         screen.appendNumber(this.innerText);
     });
+
+    // C, CE, Erase
+
+    document.querySelector('.button.clear').addEventListener('click', () => {
+        calculator.clear();
+        screen.clear();
+    });
+
+    document.querySelector('.button.clear-entry').addEventListener('click', () => {
+        screen.clearEntry();
+    });
+
+    document.querySelector('.button.erase').addEventListener('click', () => {
+        screen.erase();
+    });
+
+    // Primitive operators
+
+    document.querySelectorAll('.button.primOp').forEach(opBtn => {
+        opBtn.addEventListener('click', function(){
+            calculator.operation(this.innerText);
+        });
+    });
+
+    // Special operators
+
+    document.querySelector('.button.percentage').addEventListener('click', () => {
+        calculator.operation('%');
+    });
+
+    document.querySelector('.button.reciprocal').addEventListener('click', () => {
+        calculator.operation('rec');
+    });
+
+    document.querySelector('.button.sqr').addEventListener('click', () => {
+        calculator.operation('sqr');
+    });
+
+    document.querySelector('.button.sqrt').addEventListener('click', () => {
+        calculator.operation('sqrt');
+    });
+
+    // Sign toggle key
+
+    document.querySelector('.button.sign').addEventListener('click', () =>  {
+        screen.toggleSign();
+    });
+
+    // Equals key
+
+    document.querySelector('.button.equals').addEventListener('click', () => {
+        calculator.compute();
+    });
+    
 }
 
-// Point key
-
-document.querySelector('.button.point').addEventListener('click', function(){
-    screen.appendNumber(this.innerText);
-});
-
-// C, CE, Erase
-
-document.querySelector('.button.clear').addEventListener('click', () => {
-    calculator.clear();
-    screen.clear();
-});
-
-document.querySelector('.button.clear-entry').addEventListener('click', () => {
-    screen.clearEntry();
-});
-
-document.querySelector('.button.erase').addEventListener('click', () => {
-    screen.erase();
-});
-
-// Primitive operators
-
-document.querySelectorAll('.button.primOp').forEach(opBtn => {
-    opBtn.addEventListener('click', function(){
-        calculator.operation(this.innerText);
-    });
-});
-
-// Special operators
-
-document.querySelector('.button.percentage').addEventListener('click', () => {
-    calculator.operation('%');
-});
-
-document.querySelector('.button.reciprocal').addEventListener('click', () => {
-    calculator.operation('rec');
-});
-
-document.querySelector('.button.sqr').addEventListener('click', () => {
-    calculator.operation('sqr');
-});
-
-document.querySelector('.button.sqrt').addEventListener('click', () => {
-    calculator.operation('sqrt');
-});
-
-// Sign toggle key
-
-document.querySelector('.button.sign').addEventListener('click', () =>  {
-    screen.toggleSign();
-});
-
-// Equals key
-
-document.querySelector('.button.equals').addEventListener('click', () => {
-    calculator.compute();
-});
+main();
