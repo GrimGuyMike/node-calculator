60b7d95cc531cd3a821d1d3b636f9b4f46864d1c
GrimGuyMike <ggm_job@mail.ru>
2021-10-17 11:41:04 +0500

    created screen module

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,12 @@
+import { ScreenObserver } from "./screen.esm.js";
+
 function main() {
-    //
+    const screen = new ScreenObserver();
+
+    screen.update({
+        history: 'hello',
+        entry: 'world'
+    });
 }
 
 main();
---
+++ public/scripts/screen.esm.js
@@ -0,0 +1,20 @@
+import { Observer } from "./patterns.esm.js";
+
+export class ScreenObserver extends Observer {
+    _entry = document.querySelector('.screen > .current');
+    _history = document.querySelector('.screen > .history');
+
+    set entry(val) {
+        this._entry.innerText = val;
+    };
+
+    set history(val) {
+        this._history.innerText = val;
+    };
+
+    update(state) {
+        for(let prop in state) {
+            this[prop] = state[prop];
+        }
+    };
+};
