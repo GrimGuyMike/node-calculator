91e13525a5aa5758b90f71ca08304f27d6186ef0
GrimGuyMike <ggm_job@mail.ru>
2021-10-18 12:02:17 +0500

    added keyboard input support

--- public/scripts/inputHandler.esm.js
+++ public/scripts/inputHandler.esm.js
@@ -10,12 +10,25 @@ export class InputHandler {
     newInputEvent = new InputEvent();
 
     constructor() {
-        document.querySelectorAll('.button').forEach(button => {
+        const buttons = document.querySelectorAll('.button');
+
+        buttons.forEach(button => {
             button.addEventListener('click', e => {
                 this.newInputEvent.notify({
                     role: e.target.getAttribute('data-role'),
                     value: e.target.getAttribute('data-value')
                 });
+            });
+        });
+
+        buttons.forEach(button => {
+            button.addEventListener('keydown', e => {
+                if(e.code === 'Enter') {
+                    this.newInputEvent.notify({
+                        role: e.target.getAttribute('data-role'),
+                        value: e.target.getAttribute('data-value')
+                    });
+                }
             });
         });
     };
